924c4a2782e81aa7b644476cf13e6a2c1e9bba8a
ketdoannguyen <ketdoannguyen.ai@gmail.com>
2024-09-14 14:47:35 +0700

    locvodich

---
+++ b.py
@@ -0,0 +1,10 @@
+from monai.config import print_config
+from monai.apps import download_and_extract
+import torch
+import matplotlib.pyplot as plt
+import tempfile
+import shutil
+import os
+import glob
+
+print_config()
